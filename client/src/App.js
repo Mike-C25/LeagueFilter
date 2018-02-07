e11f4ae626352bdb0a37e3e41deb624d0dafc343
Michael Chiang <michael.chiang25@gmail.com>
2018-02-07 16:55:21 -0500

    added position select/ css changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     initialChampions: [],
     champions: [],
     users: [],
-    searchTerm: ""
+    searchTerm: "",
+    position: ""
   }
 
   componentDidMount() {
@@ -18,40 +19,82 @@ class App extends Component {
 
     fetch('/champion')
       .then(res => res.json())
-      .then(champList => this.setState({ initialChampions: champList,
-      champions: champList}));
+      .then(champList => this.setState({initialChampions: champList, champions: champList}));
     // API.getChampionData() .then(res => console.log(res)) .catch(err =>
     // console.log(err));
 
   }
 
   handleSearchInput = e => {
-    const { name, value } = e.target;
+    const {name, value} = e.target;
     //filterChampion list
-    const champions = this.state.initialChampions.filter( iC => iC.name.toLowerCase().includes(value));
-    this.setState({
-      [name]: value,
-      champions: champions
-    })
-  }
-  
+    const champions = this
+      .state
+      .initialChampions
+      .filter(iC => iC.name.toLowerCase().includes(value));
+    this.setState({[name]: value, champions: champions})
+  };
+
+  handleSelectInput = e => {
+    e.preventDefault();
+    const {value} = e.target;
+    if (value !== "all") {
+      const champions = this
+        .state
+        .initialChampions
+        .filter(iC => iC.position.includes(value.toLowerCase()));
+      this.setState({position: value, champions: champions});
+    } else {
+      this.setState({champions: this.state.initialChampions})
+    }
+
+  };
 
   render() {
     return (
       <div className="App">
-        <h2>Champion Filter</h2>
-        <span className="s-icon"></span><input className="c-search" type="search" onChange={this.handleSearchInput} value={this.state.searchTerm} name="searchTerm" placeholder="Filter Champions" />
-        <h1>Select A Champion</h1>
-        {this
-          .state
-          .champions
-          .map(champions => 
-            <div className="champion-block" key={champions.id}>
+        <h2 className="site-name">Champion Filter</h2>
+        <div className="filter-container">
+          <div className="search-container">
+            <span className="s-icon"></span>
+            <input
+              className="c-search"
+              type="search"
+              onChange={this.handleSearchInput}
+              value={this.state.searchTerm}
+              name="searchTerm"
+              placeholder="Filter Champions"/>
+          </div>
+          <div className="select-container">
+            <span>Position:
+            </span>
+            <select
+              className="position-select"
+              value={this.state.position}
+              onChange={this.handleSelectInput}>
+              <option value="all">All</option>
+              <option value="top">Top</option>
+              <option value="middle">Middle</option>
+              <option value="jungle">Jungle</option>
+              <option value="bottom">Bottom</option>
+              <option value="support">Support</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="champlist">
+          <h2>Select A Champion</h2>
+          {this
+            .state
+            .champions
+            .map(champions => <div className="champion-block" key={champions.id}>
               <div className="champion-text">
                 <h2>{champions.name}</h2>
                 <h4>{champions.title}</h4>
               </div>
             </div>)}
+        </div>
+
       </div>
     );
   }
